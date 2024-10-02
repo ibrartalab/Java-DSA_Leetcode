296bab936fc35d5c1a060bae052779c5f1871b49
IbrarKhan <ibrartalabkhan@gmail.com>
2024-10-02 21:00:41 +0500

    Add java primitive data types.

---
+++ src/Basics/JavaIdentifier.java
@@ -0,0 +1,12 @@
+package Basics;
+
+public class JavaIdentifier {
+    public static void main(String[] args) {
+        // Allowed [A-Z][a-z][0-9] $ and _
+
+        String $name = "Ibrar Khan"; //valid
+        String _cityName = "Islamabad"; //valid
+        //String 0$age = 20; //Invalid
+        String gmail$id = "mygmail@.com";//valid
+    }
+}
