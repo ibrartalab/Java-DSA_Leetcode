6fb95d319cfc430c6ae6d63d6e38c1a7de0ddbdf
IbrarKhan <ibrartalabkhan@gmail.com>
2024-10-02 21:00:08 +0500

    Add java primitive data types.

---
+++ src/Basics/VariablesDataTypes.java
@@ -0,0 +1,24 @@
+package Basics;
+
+public class VariablesDataTypes {
+    public static void main(String[] args) {
+        //string
+        String programmingLanguage = "Java";
+        //char
+        char firstAlphabit = 'A';
+        //int
+        int year = 2024;
+        //long
+        long bigNumber = 234784784;
+        //short
+        short smallNumber = 232;
+        //float
+        float pointValues = 2.3f;
+        //double
+        double largPointNumbers = 345456.22121;
+        //byte
+        byte numeric = 123;
+        //boolean
+        boolean isActive = true;
+    }
+}
