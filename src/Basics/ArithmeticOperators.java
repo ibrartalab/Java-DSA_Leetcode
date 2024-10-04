73f508bf5797b53e44e1a59336e4efb73a9a3287
IbrarKhan <ibrartalabkhan@gmail.com>
2024-10-04 21:35:40 +0500

    Arithmetic operators

---
+++ src/Basics/ArithmeticOperators.java
@@ -0,0 +1,28 @@
+package Basics;
+
+public class ArithmeticOperators {
+    public static void main(String[] args) {
+        int x = 23;
+        int y = 56;
+
+        //Addition
+        int sum = x + y;
+        System.out.println(sum);
+
+        //Subtraction
+        int sub = x - y;
+        System.out.println(sub);
+
+        //Division
+        int div = y / x;
+        System.out.println(div);
+
+        //Multiplication
+        int mul = x * y;
+        System.out.println(mul);
+
+        //Modulus
+        int mod = x % y;
+        System.out.println(mod);
+    }
+}
