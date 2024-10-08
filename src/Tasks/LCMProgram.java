6bef6727c4b9862a9c3de58c3c169cdf6c36ab79
IbrarKhan <ibrartalabkhan@gmail.com>
2024-10-08 21:42:00 +0500

    LCM Calculator

---
+++ src/Tasks/LCMProgram.java
@@ -0,0 +1,21 @@
+package Tasks;
+
+public class LCMProgram {
+    public static void main(String[] args) {
+        //Find the Least Common Multiple (LCM) of two numbers
+        int lcm = lcmCalculator(400,500);
+        System.out.println("LCM of the given numbers is: " + lcm);
+    }
+
+    //LCM Calculator
+    public static int lcmCalculator(int a, int b){
+        int i = 1;
+        while (true){
+            int factor = a * i;
+            if (factor % b == 0){
+                return factor;
+            }
+            i++;
+        }
+    }
+}
